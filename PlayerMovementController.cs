e585749cdd1300792d7c877d6de9718c6326f292
koitRK <34415413+koitRK@users.noreply.github.com>
2017-12-10 13:34:03 +0200

    Add files via upload

---
+++ PlayerMovementController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		float vertical = Input.GetAxisRaw ("Vertical");
+		rb.AddForce(horizontal, 0, vertical);
+
+	}
+}
